62887c47a2d91f4efdd87de6c7eba15014a10826
seonghyun26 <hyun11262000@gmail.com>
2021-01-31 01:57:24 +0900

    Using PropTypes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,73 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Food({ name, type }){
-  return <h3>I like {name} ({type})</h3>;
+
+function Food({ name, picture, rating }){
+  return <div>
+      <h3>I like {name}</h3>
+      <h4>Rating {rating}</h4>
+      <img src={picture} alt={name}/>
+    </div>;
 }
+
+function renderFood(item){
+  return <Food
+    key={item.id}
+    name={item.name}
+    rating={item.rating}
+    picture={item.image}
+  />
+}
+
+const foodILike= [
+  {
+    id: 1,
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    rating: 4.7
+  },
+  {
+    id: 2,
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+      rating: 4.3
+  },
+  {
+    id: 3,
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+      rating: 4.2
+  },
+  {
+    id: 4,
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+      rating: 4.6
+  },
+  {
+    id: 5,
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+      rating: 4.7
+  }
+]
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
-      <Food name="kimchi"/>
-      <Food name="rice"/>
-      <Food name="ramen" type="noodle"/>
+      <h1>Hello World!</h1>
+      {foodILike.map(renderFood)}
     </div>
   );
 }
