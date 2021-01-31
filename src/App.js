2bcec67305d04cac7ce050693d25de921ffb9f37
seonghyun26 <hyun11262000@gmail.com>
2021-02-01 01:35:38 +0900

    Ch3 complete

--- src/App.js
+++ src/App.js
@@ -1,42 +1,21 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 class App extends React.Component{
-  constructor() {
-    super();
-
-  }
-
   state = {
-    count: 0
-  };
-
-  add = () => {
-    this.setState( current => (
-      { count: current.count + 1 }
-    ));
-  };
-
-  minus = () => {
-    this.setState( current => (
-      { count: current.count - 1 }
-    ));
-  };
-
-  componentDidUpdate() {
-    console.log("componet updated")
+    isLoading: true
   }
+  
   componentDidMount(){
-    console.log("component rendered")
+    setTimeout( () => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
-
+  
   render(){
-    console.log("rendering")
+    const { isLoading } = this.state;
     return (
       <div>
-        <h1>Number: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
+        <h1>{ isLoading ? "Loading..." : "Ready!"}</h1>
       </div>
     );
   }
