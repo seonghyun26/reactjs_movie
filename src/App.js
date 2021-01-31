7535bd0b7ee3d0341edf5ff29d13ee8d6927d768
seonghyun26 <hyun11262000@gmail.com>
2021-02-01 01:21:00 +0900

    Component Life Cycle Example

--- src/App.js
+++ src/App.js
@@ -2,19 +2,36 @@ import React from "react";
 import PropTypes from "prop-types";
 
 class App extends React.Component{
+  constructor() {
+    super();
+
+  }
+
   state = {
     count: 0
   };
 
   add = () => {
-    console.log("add");
+    this.setState( current => (
+      { count: current.count + 1 }
+    ));
   };
 
   minus = () => {
-    console.log("minus")
+    this.setState( current => (
+      { count: current.count - 1 }
+    ));
   };
 
+  componentDidUpdate() {
+    console.log("componet updated")
+  }
+  componentDidMount(){
+    console.log("component rendered")
+  }
+
   render(){
+    console.log("rendering")
     return (
       <div>
         <h1>Number: {this.state.count}</h1>
