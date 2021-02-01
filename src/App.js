d6065f6b5ca5b233f5d08a51366001c4fa18cbcf
seonghyun26 <hyun11262000@gmail.com>
2021-02-01 21:03:29 +0900

    Ch4 Complete: Render & CSS

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie.js"
+import "./App.css";
 
 class App extends React.Component{
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: []
   }
-  
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+    this.setState({ movies, isLoading: false });
+  }
+
   componentDidMount(){
-    setTimeout( () => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
   
   render(){
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        <h1>{ isLoading ? "Loading..." : "Ready!"}</h1>
-      </div>
+      <section className="container">
+        { isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
+          </div>
+        ) : ( 
+          <div className="movies">
+            {movies.map( movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
