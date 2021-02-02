7ca02e63644a5dd05b99207ec6e52aef262b06c9
seonghyun26 <hyun11262000@gmail.com>
2021-02-02 23:59:11 +0900

    Navigation Built

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,10 +1,15 @@
 import React from "react";
+import "./About.css";
 
 function About() {
     return (
-        <span>
-            About Page
-        </span>
+        <div className="about__container">
+            <span>
+                “Freedom is the freedom to say that two plus two make four. If that is
+                granted, all else follows.”
+            </span>
+            <span>− George Orwell, 1984</span>
+        </div>
     )
 }
 
