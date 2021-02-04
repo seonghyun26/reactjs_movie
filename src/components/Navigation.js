83dea5c0359d171d8dd633c63a8ceab7f017991a
seonghyun26 <hyun11262000@gmail.com>
2021-02-04 16:43:24 +0900

    Redirection Set

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 import "./Navigation.css";
 
 function Navigation () {
-    return (
-        <div class="nav">
-            <Link to="/">HOME</Link>
-            <Link to="/about">About</Link>
-        </div>   
-    )
+  return (
+    <div className="nav">
+      <Link to="/">HOME</Link>
+      <Link to="/about">About</Link>
+    </div>   
+  )
 }
 
 export default Navigation
